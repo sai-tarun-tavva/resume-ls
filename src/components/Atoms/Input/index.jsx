0de4d4c6ed7a259296f612a876cccca70adcabe4
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-09-25 22:00:09 -0400

    fix: global input style

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./index.module.css";
 
-const Input = ({ label, id, error, ...props }) => {
+const Input = ({ label, id, error, children, ...props }) => {
   return (
     <div className={`${styles.control} ${error ? styles.error : ""}`}>
       <label htmlFor={id}>{label}</label>
       <input id={id} {...props} />
+      {children}
       <div className={styles["control-error"]}>
         <p>{error}</p>
       </div>
