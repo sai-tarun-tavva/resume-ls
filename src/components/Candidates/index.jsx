491cc97de0c732dfefd5d3b5ba91d034cff82433
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-09-27 15:29:13 -0400

    feat: load candidates on page load

--- src/components/Candidates/index.jsx
+++ src/components/Candidates/index.jsx
@@ -1,16 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Candidate from "./Candidate";
 import { DataContext } from "../../store/DataContextProvider";
 import { ITEMS_PER_PAGE } from "../../utilities/constants";
 import styles from "./index.module.css";
 
 const Candidates = () => {
-  const { startIndex, candidateData } = useContext(DataContext);
+  const { startIndex, candidateData, onFilteredDataChange } =
+    useContext(DataContext);
 
   const candidates = candidateData.slice(
     startIndex,
     startIndex + ITEMS_PER_PAGE
   );
+
+  useEffect(() => {
+    const fetchCandidates = async () => {
+      try {
+        const response = await fetch(
+          // Pending Update
+          "https://run.mocky.io/v3/fdedfa33-d534-407a-9755-f13407a1161b"
+        );
+
+        if (!response.ok) {
+          throw new Error(`Error fetching candidates: ${response.statusText}`);
+        }
+
+        const resData = await response.json();
+        return resData.results;
+      } catch (error) {
+        console.error(error);
+        return []; // Handle errors by returning an empty array or any error state
+      }
+    };
+
+    const getData = async () => {
+      const candidates = await fetchCandidates();
+      onFilteredDataChange(candidates);
+    };
+
+    // Fetch candidates only for the first time
+    if (candidateData.length === 0) getData();
+  }, [onFilteredDataChange, candidateData.length]);
 
   return (
     <section className={styles.cards}>
