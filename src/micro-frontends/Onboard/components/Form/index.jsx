a4688e4d76152ba6b0575767982df21f7047f3e1
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-10-17 20:13:09 -0400

    feat: add form container and progress

--- src/micro-frontends/Onboard/components/Form/index.jsx
+++ src/micro-frontends/Onboard/components/Form/index.jsx
@@ -1,11 +1,27 @@
-import FormOnboarding from "../FormOnboarding";
+import FormProgress from "../FormProgress";
+import FormSection from "../FormSection";
+import Button from "../../../Atoms/components/Button";
 import classes from "./index.module.scss";
 
 const Form = () => {
   return (
-    <form className={classes.formContainer}>
-      <FormOnboarding />
-    </form>
+    <div className={classes.formContainer}>
+      <FormProgress />
+      <form className={classes.form}>
+        <FormSection />
+        <div className={classes.actions}>
+          <Button className={classes.closeButton}>Close</Button>
+          <div className={classes.navActions}>
+            <Button className={classes.button}>
+              <i className="bi bi-caret-left-fill" />
+            </Button>
+            <Button className={classes.button}>
+              <i className="bi bi-caret-right-fill" />
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
---
+++ src/micro-frontends/Onboard/components/FormSection/index.jsx
@@ -0,0 +1,8 @@
+import classes from "./index.module.scss";
+
+const FormSection = () => {
+  return <div></div>;
+};
+
+FormSection.displayName = "FormSection";
+export default FormSection;
