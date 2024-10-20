f91eac4ff50c10660cf53e9b723df80136ad536a
Sai Tarun Tavva <saitarun.tavva@gmail.com>
2024-10-19 21:08:59 -0400

    fix: align address component

--- src/micro-frontends/Onboard/components/FormSection/Address/index.jsx
+++ src/micro-frontends/Onboard/components/FormSection/Address/index.jsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
+import { useImperativeHandle, forwardRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import FormActions from "../../FormActions";
 import InputV2 from "../../../../Atoms/components/Inputs/InputV2";
 import Select from "../../../../Atoms/components/Inputs/Select";
 import { useInput } from "../../../../Atoms/hooks";
@@ -20,7 +19,7 @@ const getStateOptions = (country) => {
   }
 };
 
-const Address = () => {
+const Address = forwardRef((_, ref) => {
   const dispatch = useDispatch();
   const {
     data: {
@@ -96,23 +95,28 @@ const Address = () => {
     resetState();
   }, [countryValue, resetState]);
 
-  // Check if section is valid based on errors
-  const isSectionValid =
-    !address1Error &&
-    !cityError &&
-    !stateError &&
-    !countryError &&
-    !zipcodeError;
+  // Group all errors and values dynamically
+  const allErrors = [
+    address1Error,
+    cityError,
+    stateError,
+    countryError,
+    zipcodeError,
+  ];
 
-  // Check if values are empty to force validation
-  const isValuesEmpty =
-    !address1Value ||
-    !cityValue ||
-    !stateValue ||
-    !countryValue ||
-    !zipcodeValue;
+  const allValues = [
+    address1Value,
+    cityValue,
+    stateValue,
+    countryValue,
+    zipcodeValue,
+  ];
 
-  // Force all validations
+  const noErrors = allErrors.every((error) => !error); // Ensure no errors
+  const allValuesPresent = allValues.every((value) => value); // Ensure all values are filled
+
+  const isSectionValid = noErrors && allValuesPresent;
+
   const forceValidations = () => {
     forceAddress1Validations();
     forceCityValidations();
@@ -121,18 +125,10 @@ const Address = () => {
     forceZipcodeValidations();
   };
 
-  const previousClickHandler = (event) => {
-    event.preventDefault();
-
-    dispatch(inputActions.decrementCurrentSectionIndex());
-  };
-
-  const nextClickHandler = (event) => {
-    event.preventDefault();
-
-    if (isValuesEmpty) {
+  const submit = () => {
+    if (!isSectionValid) {
       forceValidations();
-      return;
+      return false;
     }
 
     dispatch(
@@ -149,110 +145,107 @@ const Address = () => {
         },
       })
     );
-    dispatch(inputActions.incrementCurrentSectionIndex());
+    return true;
   };
+
+  // Expose methods to parent using ref
+  useImperativeHandle(ref, () => ({
+    submit,
+  }));
 
   return (
     <>
-      <div className={classes.addressContainer}>
-        <div className={classes.addressRow}>
-          <InputV2
-            id="currentAddress1"
-            type="text"
-            label="Address Line 1"
-            value={address1Value}
-            changeHandler={address1Change}
-            blurHandler={address1Blur}
-            focusHandler={address1Focus}
-            error={address1Error}
-            isFocused={isAddress1Focused}
-            extraClass={classes.halfInputWidth}
-            isRequired
-          />
+      <div className={classes.addressRow}>
+        <InputV2
+          id="currentAddress1"
+          type="text"
+          label="Address Line 1"
+          value={address1Value}
+          changeHandler={address1Change}
+          blurHandler={address1Blur}
+          focusHandler={address1Focus}
+          error={address1Error}
+          isFocused={isAddress1Focused}
+          extraClass={classes.halfInputWidth}
+          isRequired
+        />
 
-          <InputV2
-            id="currentAddress2"
-            type="text"
-            label="Address Line 2"
-            value={address2Value}
-            changeHandler={address2Change}
-            blurHandler={address2Blur}
-            focusHandler={address2Focus}
-            error={address2Error}
-            isFocused={isAddress2Focused}
-            extraClass={classes.halfInputWidth}
-          />
-        </div>
-
-        <div className={classes.addressRow}>
-          <InputV2
-            id="currentCity"
-            type="text"
-            label="City"
-            value={cityValue}
-            changeHandler={cityChange}
-            blurHandler={cityBlur}
-            focusHandler={cityFocus}
-            error={cityError}
-            isFocused={isCityFocused}
-            extraClass={classes.halfInputWidth}
-            isRequired
-          />
-
-          <Select
-            id="currentState"
-            label="State"
-            value={stateValue}
-            options={getStateOptions(countryValue)}
-            changeHandler={stateChange}
-            blurHandler={stateBlur}
-            focusHandler={stateFocus}
-            error={stateError}
-            isFocused={isStateFocused}
-            extraClass={classes.halfInputWidth}
-            isRequired
-          />
-        </div>
-
-        <div className={classes.addressRow}>
-          <Select
-            id="currentCountry"
-            label="Country"
-            value={countryValue}
-            options={OPTIONS.COUNTRY}
-            changeHandler={countryChange}
-            blurHandler={countryBlur}
-            focusHandler={countryFocus}
-            error={countryError}
-            isFocused={isCountryFocused}
-            extraClass={classes.halfInputWidth}
-            isRequired
-          />
-
-          <InputV2
-            id="currentZipcode"
-            type="text"
-            label="Zipcode"
-            value={zipcodeValue}
-            changeHandler={zipcodeChange}
-            blurHandler={zipcodeBlur}
-            focusHandler={zipcodeFocus}
-            error={zipcodeError}
-            isFocused={isZipcodeFocused}
-            extraClass={classes.halfInputWidth}
-            isRequired
-          />
-        </div>
+        <InputV2
+          id="currentAddress2"
+          type="text"
+          label="Address Line 2"
+          value={address2Value}
+          changeHandler={address2Change}
+          blurHandler={address2Blur}
+          focusHandler={address2Focus}
+          error={address2Error}
+          isFocused={isAddress2Focused}
+          extraClass={classes.halfInputWidth}
+        />
       </div>
 
-      <FormActions
-        isNextDisabled={!isSectionValid}
-        previousHandler={previousClickHandler}
-        nextHandler={nextClickHandler}
-      />
+      <div className={classes.addressRow}>
+        <InputV2
+          id="currentCity"
+          type="text"
+          label="City"
+          value={cityValue}
+          changeHandler={cityChange}
+          blurHandler={cityBlur}
+          focusHandler={cityFocus}
+          error={cityError}
+          isFocused={isCityFocused}
+          extraClass={classes.halfInputWidth}
+          isRequired
+        />
+
+        <Select
+          id="currentState"
+          label="State"
+          value={stateValue}
+          options={getStateOptions(countryValue)}
+          changeHandler={stateChange}
+          blurHandler={stateBlur}
+          focusHandler={stateFocus}
+          error={stateError}
+          isFocused={isStateFocused}
+          extraClass={classes.halfInputWidth}
+          isRequired
+        />
+      </div>
+
+      <div className={classes.addressRow}>
+        <Select
+          id="currentCountry"
+          label="Country"
+          value={countryValue}
+          options={OPTIONS.COUNTRY}
+          changeHandler={countryChange}
+          blurHandler={countryBlur}
+          focusHandler={countryFocus}
+          error={countryError}
+          isFocused={isCountryFocused}
+          extraClass={classes.halfInputWidth}
+          isRequired
+        />
+
+        <InputV2
+          id="currentZipcode"
+          type="text"
+          label="Zipcode"
+          value={zipcodeValue}
+          changeHandler={zipcodeChange}
+          blurHandler={zipcodeBlur}
+          focusHandler={zipcodeFocus}
+          error={zipcodeError}
+          isFocused={isZipcodeFocused}
+          extraClass={classes.halfInputWidth}
+          isRequired
+        />
+      </div>
     </>
   );
-};
+});
 
 Address.displayName = "FormAddress";
 export default Address;
